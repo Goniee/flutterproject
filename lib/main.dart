eebe9e3a3b6bef379ecba48feb71992830085d63
nguyentrangquocthinh <quocthinh1233002@gmail.com>
2024-04-23 21:47:01 +0700

    updated 9:46

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,7 @@ class MyApp extends StatelessWidget {
                 builder: (context, themeProvider, child) {
               return MaterialApp(
                 debugShowCheckedModeBanner: false,
-                title: 'BalochDev Shopping',
+                title: 'Tony Shop',
                 theme: Styles.themeData(
                     isDarkTheme: themeProvider.getIsDarkTheme,
                     context: context),
